42787b6b7b27ad8f50a473905eb2489ab5abfcfe
Yves Casio <yveschristianpcasio@gmail.com>
2026-03-07 00:39:45 +0800

    feat: test cases for linked list

---
+++ src/linkedList/linkedList.spec.js
@@ -0,0 +1,76 @@
+import { LinkedList } from "./linkedList";
+
+describe("Linked List Test", () => {
+	test("should add a new node to start", () => {
+		const ll = new LinkedList();
+		const value = 24;
+
+		ll.append(value);
+
+		expect(ll.head()).toBe(value);
+	});
+
+	test("should add a new node at end", () => {
+		const ll = new LinkedList();
+		const value = 24;
+
+		ll.append(value);
+
+		expect(ll.tail()).toBe(value);
+	});
+
+	test("at() should return the value of node at given index", () => {
+		const ll = new LinkedList();
+		const value = 24;
+
+		ll.append(21);
+		ll.append(value);
+		ll.append(23);
+
+		expect(ll.at(1)).toBe(value);
+	});
+
+	test("pop() should remove the head node", () => {
+		const ll = new LinkedList();
+		const value = 24;
+
+		ll.append(21);
+		ll.append(23);
+		ll.append(value);
+
+		expect(ll.contains(value)).toBeFalsy();
+	});
+
+	test("contains() should detect existing value in the list", () => {
+		const ll = new LinkedList();
+		const value = 24;
+
+		ll.append(21);
+		ll.append(23);
+		ll.append(value);
+
+		expect(ll.contains(value)).toBeTruthy();
+	});
+
+	test("findIndex() should return index of given value", () => {
+		const ll = new LinkedList();
+		const value = 24;
+
+		ll.append(21);
+		ll.append(23);
+		ll.append(value);
+
+		expect(ll.findIndex(value)).toBe(2);
+	});
+
+	test("toString() should give string representation of your string", () => {
+		const ll = new LinkedList();
+		const value = 24;
+
+		ll.append(21);
+		ll.append(23);
+		ll.append(value);
+
+		expect(ll.toString()).toBe("(21) -> (23) -> (24) -> null");
+	});
+});

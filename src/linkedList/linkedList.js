321cb7f162dc7cf4b81676ca27656d5d82dc80e4
Yves Casio <yveschristianpcasio@gmail.com>
2026-03-10 01:34:26 +0800

    feat:

--- src/linkedList/linkedList.js
+++ src/linkedList/linkedList.js
@@ -36,6 +36,20 @@ class LinkedList {
 		return node.value;
 	};
 
+	pop = function () {
+		if (this.size === 0) return undefined;
+
+		const removedNodeValue = this.head.value;
+		let node = this.tail;
+
+		while (node.next !== this.head) node = node.next;
+
+		this.head = node;
+		this.head.next = null;
+
+		return removedNodeValue;
+	};
+
 	contains = function (value) {
 		let node = this.tail;
 
@@ -45,6 +59,53 @@ class LinkedList {
 		}
 
 		return false;
+	};
+
+	findIndex = function (value) {
+		let node = this.tail;
+
+		let i = 0;
+		while (node !== null) {
+			if (value === node.value) return i;
+			node = node.next;
+			i++;
+		}
+
+		return -1;
+	};
+
+	toString = function () {
+		let str = "";
+		let node = this.tail;
+
+		while (node !== null) {
+			str += `( ${node.value} ) -> `;
+			node = node.next;
+		}
+
+		return `${str}null`;
+	};
+
+	insertAt = function (start, ...values) {
+		let node = this.tail;
+
+		while (start !== node.value && node.next !== null) node = node.next;
+
+		const tempNode = node.next;
+
+		values.forEach((val) => {
+			node.next = new LinkedList.Node(val, null);
+			node = node.next;
+		});
+
+		node.next = tempNode;
+	};
+
+	removeAt = function (index) {
+		if (index > this.size - 1) return undefined;
+
+		let node = this.tail;
+		for (let i = 0; i <= index && node !== null; i++) node = node.next;
 	};
 
 	initList = function (newNode) {
@@ -77,5 +138,15 @@ console.log(linkedList1.tail);
 console.log(linkedList1.size);
 console.log(linkedList1.at(2));
 console.log(linkedList1.contains(21));
+
+console.log(linkedList1.pop());
+console.log(linkedList1);
+
+console.log(linkedList1.findIndex(25));
+
+console.log(linkedList1.toString());
+
+console.log(linkedList1.insertAt(22, 21, 23, 123, 41));
+console.log(linkedList1);
 
 // export { LinkedList };
